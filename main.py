db01b47d963533b28cb0814920c7946f470adfd3
rob <robert.r.nicholls@gmail.com>
2020-11-20 14:37:57 +0000

    black formatting

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from pomegranate import (
 import pandas as pd
 import numpy as np
 
-X = pd.DataFrame({
-    'A' : [1,2,3,4,5],
-    'B' : [1,2,3,4,5]
-})
+X = pd.DataFrame({"A": [1, 2, 3, 4, 5], "B": [1, 2, 3, 4, 5]})
 
-model = NaiveBayes([NormalDistribution(5, 2), UniformDistribution(0, 10), ExponentialDistribution(1.0)])
+model = NaiveBayes(
+    [NormalDistribution(5, 2), UniformDistribution(0, 10), ExponentialDistribution(1.0)]
+)
 model.predict(np.array([[10]]))
 
-model = GeneralMixtureModel.from_samples(MultivariateGaussianDistribution, n_components=3, X=X)
-
+model = GeneralMixtureModel.from_samples(
+    MultivariateGaussianDistribution, n_components=3, X=X
+)
